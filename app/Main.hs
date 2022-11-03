2979ff624de6c9c3eacea51e454fb4abf702c901
onthepeakofnormal <gius_martin@yahoo.de>
2022-11-03 17:50:07 +0100

    main + gitignore

--- app/Main.hs
+++ app/Main.hs
@@ -351,9 +351,14 @@ run = displayTerm . alphaConv . fromChurch . reduceMany
 
 main :: IO ()
 main = do
+  putStrLn $ "Please enter a mini lambda program:"
   l <- readLn
   case parseTerm l of
     Left err -> putStrLn $ show err
     Right t -> do
-        putStrLn $ show t
-        putStrLn $ displayTerm t
+        putStrLn "Parsing the term succeeded!"
+        putStrLn "Proceeding to run it.."
+        putStrLn $ "The result:"
+        putStrLn $ run t
+        putStrLn "Proceeding to the next program.."
+        main
