f03382009f54749e95aada743aca668d28663998
polymorphicengine <gius_martin@yahoo.de>
2023-08-17 16:41:47 +0200

    add core

--- src/Zwirn/Interactive.hs
+++ src/Zwirn/Interactive.hs
@@ -1,5 +1,6 @@
 module Zwirn.Interactive
-    ( module Zwirn.Interactive.Prelude.Control
+    ( module Zwirn.Interactive.Prelude.Core
+    , module Zwirn.Interactive.Prelude.Control
     , module Zwirn.Interactive.Prelude.Hydra
     , module Zwirn.Interactive.Prelude.MiniPrelude
     , module Zwirn.Interactive.Prelude.Params
@@ -11,6 +12,7 @@ module Zwirn.Interactive
     , module Zwirn.Interactive.TidalT
     ) where
 
+import Zwirn.Interactive.Prelude.Core
 import Zwirn.Interactive.Prelude.Control
 import Zwirn.Interactive.Prelude.Hydra
 import Zwirn.Interactive.Prelude.MiniPrelude
