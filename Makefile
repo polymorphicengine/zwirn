5b3cd16d41655e97b39bf0b9693e830d48badd11
polymorphicengine <gius_martin@yahoo.de>
2025-02-07 17:08:27 +0100

    move zwirn editor to own repo

--- Makefile
+++
@@ -1,8 +0,0 @@
-execpath := $(shell cabal list-bin zwirn-editor)
-start:
-	cabal build --write-ghc-environment-files=always
-	-rm -r build
-	mkdir build
-	cp $(execpath) build
-	cp -r static build
-	npm start
--- postpack-app-win.js
+++
@@ -1,8 +0,0 @@
-const shell = require('shelljs');
-const os = require('os');
-
-const plat = os.platform();
-const arch = os.arch();
-
-shell.echo(`Running postpack app for ${plat}:${arch}`);
-shell.exec(`ncp static ThreepennyElectron-${plat}-${arch}/static`);
